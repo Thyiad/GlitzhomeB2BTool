da00f10debf1c457f77e8f2ffb00ac43c404815d
Thyiad <1520583107@qq.com>
2016-06-01 11:06:29 +0800

    本地代码提交

---
+++ GlitzhomeB2BTool/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace GlitzhomeB2BTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.ckZH = new System.Windows.Forms.CheckBox();
+            this.label2model = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFile
+            // 
+            this.txtFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFile.Location = new System.Drawing.Point(20, 62);
+            this.txtFile.Multiline = true;
+            this.txtFile.Name = "txtFile";
+            this.txtFile.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtFile.Size = new System.Drawing.Size(666, 189);
+            this.txtFile.TabIndex = 0;
+            this.txtFile.Text = resources.GetString("txtFile.Text");
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.Location = new System.Drawing.Point(307, 286);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(75, 23);
+            this.btnAnalyze.TabIndex = 3;
+            this.btnAnalyze.Text = "解析";
+            this.btnAnalyze.UseVisualStyleBackColor = true;
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
+            // 
+            // txtOut
+            // 
+            this.txtOut.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOut.Location = new System.Drawing.Point(20, 345);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOut.Size = new System.Drawing.Size(666, 196);
+            this.txtOut.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "messages folder：";
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMsg.Location = new System.Drawing.Point(140, 18);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(546, 21);
+            this.txtMsg.TabIndex = 5;
+            this.txtMsg.Text = "D:\\work\\Glitzhome-B2B\\backend\\messages";
+            // 
+            // ckZH
+            // 
+            this.ckZH.AutoSize = true;
+            this.ckZH.Checked = true;
+            this.ckZH.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ckZH.Location = new System.Drawing.Point(440, 292);
+            this.ckZH.Name = "ckZH";
+            this.ckZH.Size = new System.Drawing.Size(36, 16);
+            this.ckZH.TabIndex = 6;
+            this.ckZH.Text = "zh";
+            this.ckZH.UseVisualStyleBackColor = true;
+            this.ckZH.CheckedChanged += new System.EventHandler(this.ckZH_CheckedChanged);
+            // 
+            // label2model
+            // 
+            this.label2model.Location = new System.Drawing.Point(43, 288);
+            this.label2model.Name = "label2model";
+            this.label2model.Size = new System.Drawing.Size(209, 23);
+            this.label2model.TabIndex = 7;
+            this.label2model.Text = "label:\"xxx\" >> model::uiMsg";
+            this.label2model.UseVisualStyleBackColor = true;
+            this.label2model.Click += new System.EventHandler(this.label2model_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 562);
+            this.Controls.Add(this.label2model);
+            this.Controls.Add(this.ckZH);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAnalyze);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.txtFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.TextBox txtOut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.CheckBox ckZH;
+        private System.Windows.Forms.Button label2model;
+    }
+}
+
---
+++ GlitzhomeB2BTool/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace GlitzhomeB2BTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> keys = new List<string>();
+
+        private List<string> enKeys = new List<string>();
+        private List<string> zhKeys = new List<string>();
+
+        private void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            keys.Clear();
+
+            var txt = txtFile.Text.Trim();
+            if (string.IsNullOrEmpty(txt)) return;
+
+            var files =  Regex.Split(txt,Environment.NewLine);
+            foreach (var file in files)
+            {
+                if (Directory.Exists(file))
+                {
+                    foreach (var item in Directory.GetFiles(file))
+                    {
+                        AnalyzeFile(item);
+                    }
+                }
+                else if (File.Exists(file))
+                {
+                    AnalyzeFile(file);
+                }
+            }
+
+            Out();
+        }
+
+        private Regex regYiitapp = new Regex(@"Yii::t\('app'\s*,\s*'([^']+)'\)");
+        private Regex regModelGetuimsg = new Regex(@"[A-z]+::getUIMsg\('([^']+)'\)");
+        private void AnalyzeFile(string file)
+        {
+            foreach (var line in File.ReadAllLines(file))
+            {
+                if (regYiitapp.IsMatch(line))
+                {
+                    MatchCollection matches = regYiitapp.Matches(line);
+                    foreach (Match match in matches)
+                    {
+                        var key = match.Groups[1].Value;
+
+                        if (!keys.Contains(key)) keys.Add(key);
+                    }
+                }
+
+                if (regModelGetuimsg.IsMatch(line))
+                {
+                    MatchCollection matches = regModelGetuimsg.Matches(line);
+                    foreach (Match match in matches)
+                    {
+                        var key = match.Groups[1].Value;
+
+                        if (!keys.Contains(key)) keys.Add(key);
+                    }
+                }
+            }
+        }
+
+        private void Out()
+        {
+            InitExistKeys();
+
+            List<string> existKeys = ckZH.Checked ? zhKeys : enKeys;
+
+            txtOut.Text = "";
+            StringBuilder builder = new StringBuilder();
+            foreach (var item in keys)
+            {
+                if(!existKeys.Contains(item))
+                    builder.AppendLine(string.Format("'{0}' => '',", item));
+            }
+            txtOut.Text = builder.ToString();
+        }
+
+        private void InitExistKeys()
+        {
+            var en = txtMsg.Text.Trim() + "/en/app.php";
+            var zh = txtMsg.Text.Trim() + "/zh-CN/app.php";
+
+            Regex regex = new Regex(@"'(?'key'[^']+)'\s*=>\s*'[^']*',");
+            foreach (var line in File.ReadAllLines(en))
+            {
+                if (regex.IsMatch(line))
+                {
+                    var match = regex.Match(line);
+                    var key = match.Groups["key"].Value;
+
+                    if (!enKeys.Contains(key)) enKeys.Add(key);
+                }
+            }
+            foreach (var line in File.ReadAllLines(zh))
+            {
+                if (regex.IsMatch(line))
+                {
+                    var match = regex.Match(line);
+                    var key = match.Groups["key"].Value;
+
+                    if (!zhKeys.Contains(key)) zhKeys.Add(key);
+                }
+            }
+        }
+
+        private List<string> labels = new List<string>();
+        private void label2model_Click(object sender, EventArgs e)
+        {
+            var txt = txtFile.Text.Trim();
+            if (string.IsNullOrEmpty(txt)) return;
+
+            var files = Regex.Split(txt, Environment.NewLine);
+            foreach (var file in files)
+            {
+                if (Directory.Exists(file))
+                {
+                    foreach (var item in Directory.GetFiles(file))
+                    {
+                        AnalyzeLabel(item);
+                    }
+                }
+                else if (File.Exists(file))
+                {
+                    AnalyzeLabel(file);
+                }
+            }
+
+            // out label
+            txtOut.Text = "";
+            StringBuilder builder = new StringBuilder();
+            foreach (var item in labels)
+            {
+                builder.AppendLine(string.Format("'{0}'=>Yii::t('app','{0}'),",item));
+            }
+            txtOut.Text = builder.ToString();
+        }
+
+        private Regex regeLabel = new Regex("label:\\s*(?:'([^']+)'|\"([^ \"]+)\")\\s*,");
+        private void AnalyzeLabel(string file)
+        {
+            foreach (var line in File.ReadAllLines(file))
+            {
+                if (regeLabel.IsMatch(line))
+                {
+                    string key = regeLabel.Match(line).Groups[1].Value;
+                    if (!labels.Contains(key)) labels.Add(key);
+                }
+            }
+        }
+
+        private void ckZH_CheckedChanged(object sender, EventArgs e)
+        {
+            Out();
+        }
+    }
+}
