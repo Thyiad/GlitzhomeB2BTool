b671bb44cb0044f78fbcf05d972478576dcef6a9
Thyiad <1520583107@qq.com>
2016-06-02 16:13:42 +0800

    modify ui

--- GlitzhomeB2BTool/Form1.Designer.cs
+++ GlitzhomeB2BTool/Form1.Designer.cs
@@ -36,23 +36,27 @@
             this.txtMsg = new System.Windows.Forms.TextBox();
             this.ckZH = new System.Windows.Forms.CheckBox();
             this.label2model = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtFile
             // 
-            this.txtFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtFile.Location = new System.Drawing.Point(20, 62);
+            this.txtFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtFile.Location = new System.Drawing.Point(0, 0);
             this.txtFile.Multiline = true;
             this.txtFile.Name = "txtFile";
             this.txtFile.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtFile.Size = new System.Drawing.Size(666, 189);
+            this.txtFile.Size = new System.Drawing.Size(450, 461);
             this.txtFile.TabIndex = 0;
             this.txtFile.Text = resources.GetString("txtFile.Text");
             // 
             // btnAnalyze
             // 
-            this.btnAnalyze.Location = new System.Drawing.Point(307, 286);
+            this.btnAnalyze.Location = new System.Drawing.Point(435, 45);
             this.btnAnalyze.Name = "btnAnalyze";
             this.btnAnalyze.Size = new System.Drawing.Size(75, 23);
             this.btnAnalyze.TabIndex = 3;
@@ -62,13 +66,12 @@
             // 
             // txtOut
             // 
-            this.txtOut.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtOut.Location = new System.Drawing.Point(20, 345);
+            this.txtOut.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOut.Location = new System.Drawing.Point(0, 0);
             this.txtOut.Multiline = true;
             this.txtOut.Name = "txtOut";
             this.txtOut.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtOut.Size = new System.Drawing.Size(666, 196);
+            this.txtOut.Size = new System.Drawing.Size(441, 461);
             this.txtOut.TabIndex = 1;
             // 
             // label1
@@ -86,7 +89,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtMsg.Location = new System.Drawing.Point(140, 18);
             this.txtMsg.Name = "txtMsg";
-            this.txtMsg.Size = new System.Drawing.Size(546, 21);
+            this.txtMsg.Size = new System.Drawing.Size(767, 21);
             this.txtMsg.TabIndex = 5;
             this.txtMsg.Text = "D:\\work\\Glitzhome-B2B\\backend\\messages";
             // 
@@ -95,7 +98,7 @@
             this.ckZH.AutoSize = true;
             this.ckZH.Checked = true;
             this.ckZH.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.ckZH.Location = new System.Drawing.Point(440, 292);
+            this.ckZH.Location = new System.Drawing.Point(615, 49);
             this.ckZH.Name = "ckZH";
             this.ckZH.Size = new System.Drawing.Size(36, 16);
             this.ckZH.TabIndex = 6;
@@ -105,7 +108,7 @@
             // 
             // label2model
             // 
-            this.label2model.Location = new System.Drawing.Point(43, 288);
+            this.label2model.Location = new System.Drawing.Point(140, 45);
             this.label2model.Name = "label2model";
             this.label2model.Size = new System.Drawing.Size(209, 23);
             this.label2model.TabIndex = 7;
@@ -113,20 +116,44 @@
             this.label2model.UseVisualStyleBackColor = true;
             this.label2model.Click += new System.EventHandler(this.label2model_Click);
             // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 89);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtFile);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtOut);
+            this.splitContainer1.Size = new System.Drawing.Size(895, 461);
+            this.splitContainer1.SplitterDistance = 450;
+            this.splitContainer1.TabIndex = 8;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(717, 562);
+            this.ClientSize = new System.Drawing.Size(919, 562);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.label2model);
             this.Controls.Add(this.ckZH);
             this.Controls.Add(this.txtMsg);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnAnalyze);
-            this.Controls.Add(this.txtOut);
-            this.Controls.Add(this.txtFile);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,6 +168,7 @@
         private System.Windows.Forms.TextBox txtMsg;
         private System.Windows.Forms.CheckBox ckZH;
         private System.Windows.Forms.Button label2model;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
 
